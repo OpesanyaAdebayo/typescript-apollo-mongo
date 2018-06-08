20fbbd41be3b7d0417d6891070c1de69ba5ddf44
Opesanya Adebayo <bayoopesanya@gmail.com>
2018-06-08 22:56:38 +0100

    added schema and resolvers

---
+++ src/graphql/resolvers.ts
@@ -0,0 +1,20 @@
+export default {
+    Query: {
+      posts: async (parent, args, { Post }) => {
+         const posts = await Post.find();
+         return posts
+      },
+    },
+    Mutation: {
+      upvotePost: async (parent, args, { Post }) => {
+        const post = await Post.findOneAndUpdate({ title: args }, {$inc:{votes: 1 }}, {new: true} );
+        post._id = post._id.toString();
+        return post
+     },
+      addPost: async (parent, args, { Post }) => {
+        const post = await new Post(args).save();
+        post._id = post._id.toString();
+        return post
+     }
+    }
+  };
